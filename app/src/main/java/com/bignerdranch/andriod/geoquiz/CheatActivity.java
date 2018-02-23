d8d18c0f0dc5897725e455e343abf084a0abd7d2
Angel Rodriguez <arrmixer@gmail.com>
2018-02-23 09:48:42 -0500

    finish chapter 6

--- app/src/main/java/com/bignerdranch/andriod/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/CheatActivity.java
@@ -1,9 +1,13 @@
 package com.bignerdranch.andriod.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -37,6 +41,25 @@ public class CheatActivity extends AppCompatActivity {
                 mTextAnswer.setText(R.string.false_button);
             }
                 setExtraAnswerShown(true);
+
+            int cx = mShowAnswerButton.getWidth() / 2;
+            int cy = mShowAnswerButton.getHeight() / 2;
+            float radius = mShowAnswerButton.getWidth();
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation, boolean isReverse) {
+                        super.onAnimationEnd(animation);
+                        mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();
+            }else{
+                mShowAnswerButton.setVisibility(View.INVISIBLE);
+            }
+
             }
 
         });
