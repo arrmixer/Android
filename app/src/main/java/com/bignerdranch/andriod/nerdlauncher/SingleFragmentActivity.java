ff7da082ec744412e03bcd71b8b82e70a0830928
Angel Rodriguez <arrmixer@gmail.com>
2018-04-16 12:38:50 -0400

    New App to show Intents

---
+++ app/src/main/java/com/bignerdranch/andriod/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.andriod.nerdlauncher;
+
+
+
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Angel on 3/2/18.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected  int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
