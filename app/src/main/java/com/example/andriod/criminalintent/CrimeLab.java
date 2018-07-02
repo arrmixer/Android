43f5161407ca35a99ff8a709d58b9f5060c3e6a0
Angel Rodriguez <arrmixer@gmail.com>
2018-07-02 06:47:55 -0400

    add chapter13

---
+++ app/src/main/java/com/example/andriod/criminalintent/CrimeLab.java
@@ -0,0 +1,52 @@
+package com.example.andriod.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Angel on 3/1/18.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
+    }
+
+    public void deleteCrime(Crime c) {
+        mCrimes.remove(c);
+    }
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
+
+}
